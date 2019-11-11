c4bd6522c5ea2093da482e529579016c81f5b438
kimtahen <kimtahen@naver.com>
2019-11-11 13:20:59 +0900

    beforrouter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css'
 import Header from './Components/Header'
 import Sidebar from './Components/Sidebar'
+import Contents from './Components/Contents'
 function App(){
 	return (
 		<div>
 			<Header />
 			<Sidebar />
+			<Contents />
+
 		</div>
 	)
 }
---
+++ src/Components/Contents.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Contents.css'
+function Contents(){
+    return (
+        <div className='Contents'>
+        </div>
+    )
+}
+export default Contents;
